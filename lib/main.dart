79fa56278f3d0a4f8999553d88a7ec84ea9e3a00
amir <mohsenzahed077@gmail.com>
2023-01-23 11:26:01 +0430

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:my_id_card/size_config.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 void main() {
   runApp(
